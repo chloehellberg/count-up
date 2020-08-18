9e50f6e4ca643b72da8fc81bbc9d1748a7765c7c
William Donovan-Seid <wdstwo@live.com>
2020-08-18 10:37:27 -0700

    add fill in for loop and add if statements
    co-author: Chloe Hellberg <chloe.hellberg@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,12 @@ $(document).ready(function() {
     const countTo = parseInt($("input#countTo").val());
     const countBy = parseInt($("input#countBy").val());
 
-    let answer = [];
-
-    for (let index=countBy; index <= countTo; index+=countBy) {
-      console.log(index);
+    if (countTo && countBy && countTo>0 && countBy>0) {
+      for (let index=countBy; index <= countTo; index+=countBy) {
+        $("#overHere").append(" " + index);
+      }
+    } else {
+      alert("Please enter a number.")
     }
-
-    $("#story").show();
-
   });
 })
